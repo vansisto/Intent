9b55c49177a1277d49d9f9dee055447c204adb03
VanSisto <vansisto@gmail.com>
2020-10-15 20:48:39 +0300

    D3-JS: Started work. By 2.10 is done.

--- Java/D3/src/com/vansisto/Intent/Main.java
+++ Java/D3/src/com/vansisto/Intent/Main.java
@@ -1,8 +1,28 @@
 package com.vansisto.Intent;
 
-public class Main {
+import java.util.Arrays;
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+public class Main {
+//############     Office    ######################
+//    public static void main(String[] args) {
+//        int [] arr = {9, 1, 8, 2, 7, 3, 6, 4, 5};
+//
+//        boolean done = false;
+//        int temp;
+//        while(!done) {
+//            done = true;
+//            for (int i = 0; i < arr.length-1; i++) {
+//                if(arr[i] > arr[i+1]){
+//                    done = false;
+//
+//                    temp = arr[i];
+//                    arr[i] = arr[i+1];
+//                    arr[i+1] = temp;
+//                }
+//            }
+//        }
+//        System.out.println(Arrays.toString(arr));
+//    }
+
+    
 }
