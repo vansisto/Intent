48cfbe8f28531b988189a7599f8f5bf3a30cfd67
VanSisto <vansisto@gmail.com>
2020-10-22 20:49:36 +0300

    Java. Aleksander: LinkedList<> with streams. Some formating fixed

--- Java/D5/src/com/vansisto/Intent/LinkedList.java
+++ Java/D5/src/com/vansisto/Intent/LinkedList.java
@@ -23,26 +23,6 @@ public class LinkedList<E> implements List<E> {
     }
 
     @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
-    public Iterator iterator() {
-        return null;
-    }
-
-    @Override
-    public void forEach(Consumer<? super E> action) {
-
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
     public boolean add(E e) {
         if (last != null){
             last.next = new Node((E)e, null, last);
@@ -55,36 +35,6 @@ public class LinkedList<E> implements List<E> {
             size++;
             return true;
         }
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean removeIf(Predicate<? super E> filter) {
-        return false;
-    }
-
-    @Override
-    public void replaceAll(UnaryOperator<E> operator) {
-
-    }
-
-    @Override
-    public void sort(Comparator<? super E> c) {
-
-    }
-
-    @Override
-    public boolean addAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(int index, Collection c) {
-        return false;
     }
 
     @Override
@@ -105,15 +55,6 @@ public class LinkedList<E> implements List<E> {
             throw new LinkedIndexOutOfBoundException(index);
     }
 
-    @Override
-    public E set(int index, E element) {
-        return null;
-    }
-
-    @Override
-    public void add(int index, E element) {
-
-    }
 
     @Override
     public E remove(int index) {
@@ -153,65 +94,30 @@ public class LinkedList<E> implements List<E> {
         return removed.value;
     }
 
-    @Override
-    public int indexOf(Object o) {
-        return 0;
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        return 0;
-    }
-
-    @Override
-    public ListIterator listIterator() {
-        return null;
-    }
-
-    @Override
-    public ListIterator listIterator(int index) {
-        return null;
-    }
-
-    @Override
-    public List subList(int fromIndex, int toIndex) {
-        return null;
-    }
-
-    @Override
-    public Spliterator<E> spliterator() {
-        return null;
-    }
-
-    @Override
-    public Stream<E> stream() {
-        return null;
-    }
-
-    @Override
-    public Stream<E> parallelStream() {
-        return null;
-    }
-
-    @Override
-    public boolean retainAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public Object[] toArray(Object[] a) {
-        return new Object[0];
-    }
+    @Override public int indexOf(Object o) {return 0;}
+    @Override public int lastIndexOf(Object o) {return 0;}
+    @Override public ListIterator listIterator() {return null;}
+    @Override public ListIterator listIterator(int index) {return null;}
+    @Override public List subList(int fromIndex, int toIndex) {return null;}
+    @Override public Spliterator<E> spliterator() {return null;}
+    @Override public Stream<E> stream() {return null;}
+    @Override public Stream<E> parallelStream() {return null;}
+    @Override public boolean retainAll(Collection c) {return false;}
+    @Override public boolean removeAll(Collection c) {return false;}
+    @Override public boolean containsAll(Collection c) {return false;}
+    @Override public Object[] toArray(Object[] a) {return new Object[0];}
+    @Override public boolean contains(Object o) {return false;}
+    @Override public Iterator iterator() {return null;}
+    @Override public void forEach(Consumer<? super E> action) {}
+    @Override public Object[] toArray() {return new Object[0];}
+    @Override public E set(int index, E element) {return null;}
+    @Override public void add(int index, E element) {}
+    @Override public boolean remove(Object o) {return false;}
+    @Override public boolean removeIf(Predicate<? super E> filter) {return false;}
+    @Override public void replaceAll(UnaryOperator<E> operator) {}
+    @Override public void sort(Comparator<? super E> c) {}
+    @Override public boolean addAll(Collection c) {return false;}
+    @Override public boolean addAll(int index, Collection c) {return false;}
 
     public class Node {
         private E value;
